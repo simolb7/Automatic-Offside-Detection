9ae629e16d65e5db2f8ad7d44f76e9b410ac8979
Simon <labella.1995847@studenti.uniroma1.it>
2024-05-15 11:42:22 +0200

    Modello player

--- prova.py
+++ prova.py
@@ -22,4 +22,21 @@ print(int(vertical_vanishing_point[0]) , int(vertical_vanishing_point[1]))
 cv2.imwrite(vanishing_point_viz_base_path+nomeFile, imageForVanishingPoints)
 '''
 
+<<<<<<< Updated upstream
 print('ciao')
+=======
+    # Calcola la matrice di omografia
+    H, _ = cv2.findHomography(src_points, dst_points)
+
+    # Applica la trasformazione di omografia
+    rectified_image = cv2.warpPerspective(image, H, (image.shape[1], image.shape[0]))
+
+    return rectified_image
+
+# Esempio di utilizzo
+# Leggi l'immagine e individua le linee verticali
+vertical_lines = [[x1, y1], [x2, y2], ...]  # Lista dei punti delle linee verticali
+
+# Rettifica le linee verticali
+rectified_image = rectify_vertical_lines(imageForVanishingPoints, vertical_lines)
+>>>>>>> Stashed changes
